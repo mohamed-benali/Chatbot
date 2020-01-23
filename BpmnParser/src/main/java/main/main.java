e3173192d51aac91e0925e6f3c3ac46f2d735244
cg3mbenali@gmail.com <cg3mbenali@gmail.com>
2020-01-23 18:32:33 +0100

    Extend testing BPMN + Basic Querys

--- BpmnParser/src/main/java/main/main.java
+++ BpmnParser/src/main/java/main/main.java
@@ -12,7 +12,14 @@ import org.camunda.bpm.model.xml.type.*;
 import java.io.File;
 import java.util.Collection;
 
+
+
+
 public class main {
+
+    public static void println(String s) {
+        System.out.println(s);
+    }
 
     public static void main(String[] args) {
         String packagePath = "./src/main/java/main";
@@ -24,11 +31,56 @@ public class main {
         BpmnModelInstance modelInstance = Bpmn.readModelFromFile(file);
 
 
-        Collection<Task> taskInstances = modelInstance.getModelElementsByType(Task.class);
+        Collection<StartEvent> StartEventInstances = modelInstance.getModelElementsByType(StartEvent.class);
+        println("\nStartEvents");
+        for(StartEvent startEvent : StartEventInstances) {
+            String name = startEvent.getName();
+            println("Name: " + name);
+        }
 
+        println("\nParticipants");
+        Collection<Participant> ParticipantInstances = modelInstance.getModelElementsByType(Participant.class);
+        for(Participant participant : ParticipantInstances) {
+            String name = participant.getName();
+            if(name != null) println("Name: " + name);
+        }
+
+
+        println("\nMessageFlow");
+        Collection<MessageFlow> messageFlowInstances = modelInstance.getModelElementsByType(MessageFlow.class);
+        for(MessageFlow messageFlow : messageFlowInstances) {
+            String source = messageFlow.getSource().getId();
+            String target = messageFlow.getTarget().getId();
+            println("source: " + source + "   " + "target: " + target);
+        }
+
+        println("\nTasks");
+        Collection<Task> taskInstances = modelInstance.getModelElementsByType(Task.class);
         for(Task task : taskInstances) {
             String name = task.getName();
-            System.out.println("Name: " + name);
+            println("Name: " + name);
+        }
+
+        println("\nSequenceFlow");
+        Collection<SequenceFlow> sequenceFlowInstances = modelInstance.getModelElementsByType(SequenceFlow.class);
+        for(SequenceFlow sequenceFlow : sequenceFlowInstances) {
+            String name = sequenceFlow.getName();
+            if(name != null) println("Name: " + name);
+        }
+        println("\nExclusiveGateway");
+        Collection<ExclusiveGateway> exclusiveGatewayInstances = modelInstance.getModelElementsByType(ExclusiveGateway.class);
+        for(ExclusiveGateway exclusiveGateway : exclusiveGatewayInstances) {
+            String name = exclusiveGateway.getName();
+            if(name != null) println("Name: " + name);
+        }
+
+
+
+        Collection<EndEvent> EndEventInstances = modelInstance.getModelElementsByType(EndEvent.class);
+        println("\nEnd");
+        for(EndEvent endEvent : EndEventInstances) {
+            String name = endEvent.getName();
+            println("Name: " + name);
         }
 
     }
